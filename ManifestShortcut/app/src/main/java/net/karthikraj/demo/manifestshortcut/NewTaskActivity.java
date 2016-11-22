56a117c904876ab0dd6837bf756e056d4654d7cc
karthik <raj.karthik777@gmail.com>
2016-11-23 00:33:42 +0530

    Added images and decent look for tasks

--- ManifestShortcut/app/src/main/java/net/karthikraj/demo/manifestshortcut/NewTaskActivity.java
+++ ManifestShortcut/app/src/main/java/net/karthikraj/demo/manifestshortcut/NewTaskActivity.java
@@ -2,6 +2,8 @@ package net.karthikraj.demo.manifestshortcut;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Created by karthik on 27/10/16.
@@ -13,5 +15,12 @@ public class NewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_new_task, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
